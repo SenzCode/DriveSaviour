cab7c0b7e701c9fa2154a4327d7e25b75575003f
itshirushan <Tharu@ASUS-LAPTOP.(none)>
2024-09-03 14:47:57 +0530

    shop owner css

---
+++ vehicle_owner/footer/footer.php
@@ -0,0 +1,24 @@
+<footer class="site-footer">
+        <div class="footer-content">
+            <div class="footer-left">
+                <img src="../../img/ss.png" alt="Drive Saviour Logo" class="footer-logo">
+                <p>The centre of Sri Lankan auto repair. We spearhead Sri Lanka's auto care industry's digital transformation.</p>
+            </div>
+            <div class="footer-center">
+                <div class="social-media">
+                    <a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                    <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <a href="#" target="_blank"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+            <div class="footer-right">
+                <p><i class="fas fa-phone"></i> +94704633566</p>
+                <p><i class="fas fa-envelope"></i> drivesaviourlk@gmail.com</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>Copyright &copy;2024 SenzCode</p>
+        </div>
+</footer>
+
