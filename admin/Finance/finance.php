3d1c4d1e5631aedd301bb7b6cee167cfe11ef625
abhitha@AV <abhithavishvajith@gmail.com>
2024-11-06 10:53:38 +0530

    finance page created in the admin section

---
+++ admin/Finance/finance.php
@@ -0,0 +1,17 @@
+<?php
+    require '../navbar/navbar.php';
+    require '../../connection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Finance</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../navbar/style.css">
+</head>
+<body>
+    <p>this is the finance section</p>
+</body>
+</html>
