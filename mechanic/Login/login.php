cdd48d020b36f6d44da5a07dcea6fdff9cbe3663
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2024-11-17 17:09:10 +0530

    update

--- mechanic/Login/login.php
+++ mechanic/Login/login.php
@@ -6,7 +6,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$message = ""; // Initialize message variable to store feedback
+$message = "";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['login'])) {
